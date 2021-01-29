9a8d7256d69af584657db8090e05ab0c920a9c6c
Paul Realpe <paulmrg461@gmail.com>
2021-01-29 07:31:40 -0500

    64. ChatPage initial design

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,9 +4,51 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Chat Page'),
+      appBar: _myAppBar(),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+                child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (_, i) => Text('$i'),
+              reverse: true,
+            )),
+            Divider(
+              height: 1,
+            ),
+            //TODO: TextBox
+            Container(
+              color: Colors.white,
+              height: 50.0,
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  AppBar _myAppBar() {
+    return AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+                maxRadius: 16,
+                backgroundColor: Colors.blue[100],
+                child: Text(
+                  'Na',
+                  style: TextStyle(fontSize: 12.0),
+                )),
+            SizedBox(
+              width: 12.0,
+            ),
+            Text(
+              'Natalia Flores',
+              style: TextStyle(color: Colors.black87, fontSize: 14.0),
+            ),
+          ],
+        ));
   }
 }

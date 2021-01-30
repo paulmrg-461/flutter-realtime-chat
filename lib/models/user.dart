8cc0f034674cd1c58eb351ac9a27aefdd732d099
Paul Realpe <paulmrg461@gmail.com>
2021-01-30 10:15:50 -0500

    92. Map login response

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,37 @@
+// To parse this JSON data, do
+//
+//     final user = userFromJson(jsonString);
+
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
 class User {
+  User({
+    this.online,
+    this.name,
+    this.email,
+    this.uid,
+  });
+
   bool online;
-  String email;
   String name;
+  String email;
   String uid;
 
-  User({this.online, this.email, this.name, this.uid});
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        online: json["online"],
+        name: json["name"],
+        email: json["email"],
+        uid: json["uid"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "online": online,
+        "name": name,
+        "email": email,
+        "uid": uid,
+      };
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+
 import 'package:http/http.dart' as http;
+
 import 'package:realtime_chat/global/enviroment.dart';
+import 'package:realtime_chat/models/login_response.dart';
+import 'package:realtime_chat/models/user.dart';
 
 class AuthService with ChangeNotifier {
-  //User user;??
+  User user;
 
   Future login(String email, String password) async {
     final data = {'email': email, 'password': password};
@@ -14,5 +17,9 @@ class AuthService with ChangeNotifier {
         body: jsonEncode(data), headers: {'Content-Type': 'application/json'});
 
     print(resp.body);
+    if (resp.statusCode == 200) {
+      final loginResponse = loginResponseFromJson(resp.body);
+      this.user = loginResponse.user;
+    }
   }
 }
